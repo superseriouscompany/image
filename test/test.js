e7e086ad510750cddd3af6455f0b04087dc0429f
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-12-16 20:41:11 +0000

    Checkpoint: define thumbnailer API

--- test/test.js
+++ test/test.js
@@ -1,7 +1,12 @@
-const expect = require('expect');
+const expect      = require('expect');
+const fs          = require('fs');
+const thumbnailer = require('../lib/thumbnailer');
 
-describe('it', function() {
+describe('thumbnailer', function() {
   it("works", function () {
-    expect(true).toEqual(true);
+    return thumbnailer.process('./fixtures/tall.jpg').then(function(path) {
+      expect(path).toExist('No path returned from thumbnailer');
+      expect(fs.existsSync(path)).toExist(`File not found at: ${path}`);
+    })
   });
 })
