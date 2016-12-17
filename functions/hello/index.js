be07a3d95e959b43e9d823130e14bde0a96fdfca
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-12-17 00:51:33 +0000

    Checkpoint: use url from request body

--- functions/hello/index.js
+++ functions/hello/index.js
@@ -6,12 +6,16 @@ console.log('starting function')
 exports.handle = function(e, ctx, cb) {
   console.log('processing event: %j', e)
 
-  thumbnailer.process('https://s3-us-west-2.amazonaws.com/giggles-production-submissions/a1c043a0-b990-11e6-a32f-9356c83e3bd9.jpg').then(function(stream) {
+  if( !e.url ) {
+    return cb(new Error('No url provided'));
+  }
+
+  thumbnailer.process(e.url).then(function(stream) {
     var params = {Bucket: 'image.superseriouscompany.com', Key: 'lambda.png', Body: stream, ACL: 'public-read', ContentType: 'image/png'}
 
     s3.upload(params, function(err, data) {
       if( err ) { return cb(err); }
-      return cb(null, data);
+      return cb(null, {url: data.Location, event: e});
     });
   })
 }
